417f4f6fc1ce37003247e45299ea436e8d2b7482
Jelmer van der Meer <jelmervandermeer02@gmail.com>
2025-02-19 16:39:24 +0100

    Commited

---
+++ javascript/Jobjecten.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', 
+function(event){
+
+    document.getElementById('knopVaRuimtenevel').style.visibility = 'visible';
+    document.getElementById('knopAvRuimtenevel').style.visibility = 'visible';
+  
+    document.getElementById('knopVaRuimtenevel').onclick = function() {
+    document.getElementById('flipkaart-binnen').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvRuimtenevel').onclick = function() {
+    document.getElementById('flipkaart-binnen').classList.toggle('do-flip');
+    };
+
+    document.getElementById('knopVaPlaneten').style.visibility = 'visible';
+    document.getElementById('knopAvPlaneten').style.visibility = 'visible';
+  
+    document.getElementById('knopVaPlaneten').onclick = function() {
+    document.getElementById('flipkaart-binnen2').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvPlaneten').onclick = function() {
+    document.getElementById('flipkaart-binnen2').classList.toggle('do-flip');
+    };
+    document.getElementById('knopVaSterren').style.visibility = 'visible';
+    document.getElementById('knopAvSterren').style.visibility = 'visible';
+  
+    document.getElementById('knopVaSterren').onclick = function() {
+    document.getElementById('flipkaart-binnen3').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvSterren').onclick = function() {
+    document.getElementById('flipkaart-binnen3').classList.toggle('do-flip');
+    };
+    document.getElementById('knopVaMeteorieten').style.visibility = 'visible';
+    document.getElementById('knopAvMeteorieten').style.visibility = 'visible';
+  
+    document.getElementById('knopVaMeteorieten').onclick = function() {
+    document.getElementById('flipkaart-binnen4').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvMeteorieten').onclick = function() {
+    document.getElementById('flipkaart-binnen4').classList.toggle('do-flip');
+    };
+    document.getElementById('knopVaKometen').style.visibility = 'visible';
+    document.getElementById('knopAvKometen').style.visibility = 'visible';
+  
+    document.getElementById('knopVaKometen').onclick = function() {
+    document.getElementById('flipkaart-binnen5').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvKometen').onclick = function() {
+    document.getElementById('flipkaart-binnen5').classList.toggle('do-flip');
+    };
+    document.getElementById('knopVaManen').style.visibility = 'visible';
+    document.getElementById('knopAvManen').style.visibility = 'visible';
+  
+    document.getElementById('knopVaManen').onclick = function() {
+    document.getElementById('flipkaart-binnen6').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvManen').onclick = function() {
+    document.getElementById('flipkaart-binnen6').classList.toggle('do-flip');
+    };
+    document.getElementById('knopVaZG').style.visibility = 'visible';
+    document.getElementById('knopAvZG').style.visibility = 'visible';
+  
+    document.getElementById('knopVaZG').onclick = function() {
+    document.getElementById('flipkaart-binnen7').classList.toggle('do-flip');
+    };
+  
+    document.getElementById('knopAvZG').onclick = function() {
+    document.getElementById('flipkaart-binnen7').classList.toggle('do-flip');
+    };
+  }
+)
+    
+
+
+
+
+
+ 
